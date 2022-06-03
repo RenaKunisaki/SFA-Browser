5d9cf314b0dc37f72f893efdcce139f4b80c9ebd
Rena Kunisaki <hyperhacker@gmail.com>
2022-06-03 15:02:28 -0400

    texture list can scroll

--- r/js/app/ui/MapViewer/TextureViewer.js
+++ r/js/app/ui/MapViewer/TextureViewer.js
@@ -26,41 +26,45 @@ export default class TextureViewer {
     refresh() {
         if(!this.element.open) return; //don't redraw when hidden
 
-        //lol
-        const canvas = this.canvas;
-        const cw = this._body.clientWidth, ch = this._body.clientHeight;
-        canvas.setAttribute('width',   cw);
-        canvas.setAttribute('height',  ch);
-        console.log("set canvas size", cw, ch);
-
         const map  = this.mapViewer.map;
         const game = this.game;
-        const ctx  = canvas.getContext('2d');
-
-        let x=0, y=0, rowH=0;
+        const canvas = this.canvas;
+        let cw = this._body.clientWidth, ch = this._body.clientHeight;
         const pad = this.padding;
-        const lw = 1; //line width
-        ctx.lineWidth = lw;
-        ctx.strokeStyle = '#FFF';
+        const lw = 1; //line width for outline
 
+        //calculate needed size for canvas
+        let x=pad, y=pad, rowH=0;
         for(const [id, tex] of Object.entries(game.loadedTextures)) {
             if(x + tex.width + (lw*2) >= cw) {
-                x = 0;
+                x = pad;
                 y += rowH + pad + (lw*2);
-                if(y >= ch) break;
                 rowH = 0;
             }
-            ctx.strokeRect(x, y, tex.width+(lw*2), tex.height+(lw*2));
-            ctx.putImageData(tex.image._data, x+lw, y+lw);
-
             //record position so we can click on the image
             this.texturePositions[id] = {
                 x1: x+lw, y1: y+lw,
                 x2: x+tex.width+(lw*2), y2: y+tex.height+(lw*2),
             };
-
             x += tex.width + pad + (lw*2);
             rowH = Math.max(rowH, tex.height);
+        }
+
+        //set the canvas size
+        ch = y + rowH;
+        canvas.setAttribute('width',   cw);
+        canvas.setAttribute('height',  ch);
+        console.log("set canvas size", cw, ch);
+
+        //draw the images
+        const ctx  = canvas.getContext('2d');
+        ctx.lineWidth = lw;
+        ctx.strokeStyle = '#FFF';
+        for(const [id, tex] of Object.entries(game.loadedTextures)) {
+            let x = this.texturePositions[id].x1;
+            let y = this.texturePositions[id].y1;
+            ctx.strokeRect(x-1, y-1, tex.width+(lw*2), tex.height+(lw*2));
+            ctx.putImageData(tex.image._data, x+lw, y+lw);
         }
     }
 }
