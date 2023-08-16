54a53c032a5d751053074d25ae2f9c26fa7e0141
Rena Kunisaki <i.am.inuyasha@gmail.com>
2023-08-16 15:30:09 -0400

    add a bunch of doc comments

--- r/js/game/map/Map.js
+++ r/js/game/map/Map.js
@@ -14,36 +14,175 @@ import Block from "./Block.js";
  *   file but no asset directory.
  */
 export default class Map {
+
+    /** The Game instance.
+     *  @type {Game}
+     */
+    game;
+
+    /** The map's ID.
+     *  @type {number}
+     */
+    id;
+
+    /** The index into the map directory list.
+     *  @type {number}
+     */
+    dirId;
+
+    /** The name of the map's asset directory.
+     *  @type {string}
+     */
+    dirName; //XXX should be automatic from dirId
+
+    /** The name of the romlist file, found in the disc root.
+     *  @type {string}
+     */
+    romListName;
+
+    /** The number of bytes allocated for the romlist data.
+     *  @type {number}
+     */
+    romListSize;
+
+    /** The romlist instance.
+     *  @type {RomList}
+     */
+    romList;
+
+    /** The internal name from MAPINFO.bin.
+     *  @type {string}
+     */
+    name;
+
+    /** The map type ID from MAPINFO.bin.
+     *  @type {number}
+     */
+    type;
+
+    /** The map's parent ID.
+     *  @type {number}
+     */
+    parentId;
+
+    /** The linked map IDs.
+     *  @type {Array[number]}
+     */
+    links;
+
+    /** The player object type ID.
+     *  @type {number}
+     *  @note Not used in final version of the game, but still present.
+     */
+    objType;
+
+    /** The X coordinate on the global map grid, in global grid coordinates.
+     *  @type {number}
+     */
+    worldX;
+
+    /** The Z coordinate on the global map grid, in global grid coordinates.
+     *  @type {number}
+     */
+    worldZ;
+
+    /** Which global map grid this map is on.
+     *  @type {number}
+     */
+    layer;
+
+    /** The map's X size, in blocks.
+     *  @type {number}
+     */
+    sizeX;
+
+    /** The map's Z size, in blocks.
+     *  @type {number}
+     */
+    sizeZ;
+
+    /** The X coordinate of the origin block, relative to top left of map.
+     *  @type {number}
+     */
+    originX;
+
+    /** The Z coordinate of the origin block, relative to top left of map.
+     *  @type {number}
+     */
+    originZ;
+
+    /** The map's blocks.
+     *  @type {Array[Block]}
+     */
+    blocks;
+
+    /** Unknown value from MAPS.bin first struct.
+     *  @type {number}
+     */
+    unk08;
+
+    /** Unknown value from MAPS.bin first struct.
+     *  @type {number}
+     */
+    unk0C;
+
+    /** Unknown value from MAPS.bin first struct.
+     *  @type {number}
+     */
+    unk1D;
+
+    /** Unknown value from MAPS.bin first struct.
+     *  @type {number}
+     */
+    unk1E;
+
+    //following are added manually, not present in the game data
+
+    /** Brief description of which map this is.
+     *  @type {string}
+     */
+    description;
+
+    /** Whether this map is used in the game.
+     *  @type {boolean}
+     */
+    isUsed;
+
     constructor(game, params={}) {
         this.game        = assertType(game, Game);
-        this.id          = params.id;          //the map ID
-        this.dirId       = params.dirId;       //index into list of map dirs
-        this.dirName     = params.dirName;     //name of asset dir
-        this.romListName = params.romListName; //name of romlist file
-        this.romListSize = params.romListSize; //size to allocate for romlist
-        this.romList     = params.romList;     //the RomList instance
-        this.name        = params.name;        //internal name from MAPINFO.bin
-        this.type        = params.type;        //map type from MAPINFO.bin
-        this.parentId    = params.parentId;    //parent map ID
-        this.links       = params.links;       //linked map IDs
-        this.objType     = params.objType;     //player object type (not used in final)
-        this.worldX      = params.worldX;      //coords on global map grid
+        this.id          = params.id;
+        this.dirId       = params.dirId;
+        this.dirName     = params.dirName;
+        this.romListName = params.romListName;
+        this.romListSize = params.romListSize;
+        this.romList     = params.romList;
+        this.name        = params.name;
+        this.type        = params.type;
+        this.parentId    = params.parentId;
+        this.links       = params.links;
+        this.objType     = params.objType;
+        this.worldX      = params.worldX;
         this.worldZ      = params.worldZ;
-        this.layer       = params.layer;       //which global map grid
-        this.sizeX       = params.sizeX;       //in blocks
+        this.layer       = params.layer;
+        this.sizeX       = params.sizeX;
         this.sizeZ       = params.sizeZ;
-        this.originX     = params.originX;     //which block is the origin
+        this.originX     = params.originX;
         this.originZ     = params.originZ;
-        this.blocks      = params.blocks;      //list of blocks
+        this.blocks      = params.blocks;
         this.unk08       = params.unk08;
         this.unk0C       = params.unk0C;
         this.unk1D       = params.unk1D;
         this.unk1E       = params.unk1E;
-        //following are added manually, not present in the game data
-        this.description = params.description; //description
-        this.isUsed      = params.isUsed;      //is used in game?
+        this.description = params.description;
+        this.isUsed      = params.isUsed;
+
+        //these are only populated when needed
+        this.textures    = null;
     }
 
+    /** The global coordinates of this map's origin block.
+     *  @type {Array[number]}
+     */
     get worldOrigin() {
         return [
             (this.worldX - this.originX) * MAP_CELL_SIZE,
@@ -51,13 +190,22 @@ export default class Map {
         ];
     }
 
+    /** Get the block at this location.
+     *  @param {number} x The X grid coordinate relative to top left.
+     *  @param {number} z The Z grid coordinate relative to top left.
+     *  @returns {?Block} The block, or null if no block is here.
+     */
     getBlock(x, z) {
         if(this.blocks == undefined) return null;
         let idx = (z * this.sizeX) + x;
         return this.blocks[idx];
     }
 
-    addBlock(block) {
+    /** Append a block to this map's list.
+     *  @param {Block} block The block.
+     *  @note Intended to be used during parsing.
+     */
+    addBlock(block) { //XXX rename
         assertType(block, Block);
         if(this.blocks == undefined) this.blocks = [];
         this.blocks.push(block);
